c3ca487061dfdcac6faf3ca7f6b1b106202840b9
Somfic <somfic@gmail.com>
2020-11-23 18:14:32 +0100

    Created clientside

---
+++ client/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div class="scheduled">
+      <Scheduled
+        img="https://cdn.vox-cdn.com/thumbor/OMivnqFLg4XQWkGvX_LXVGJ-CtY=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/21857772/tenet_poster.jpg"
+        title="Tenet"
+        start_time="12:35"
+        end_time="14:21"
+        theatre="4"
+      ></Scheduled>
+    </div>
+    <div class="checkout">
+      <checkout></checkout>
+    </div>
+  </div>
+</template>
+
+<script>
+import Scheduled from "./components/Scheduled.vue";
+
+export default {
+  name: "App",
+  components: {
+    Scheduled,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ client/src/components/Ticket.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ticket">
+      <div>
+      <p>{{name}}</p>
+      <p>{{cost}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ticket",
+  props: {
+      name: String,
+    cost: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.ticket {
+    background-color: rgb(172, 172, 172);
+    width: 100px;
+    height: 100px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
