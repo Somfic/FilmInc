739dfe83c9804b3c0d8e5f45a7b732c9dbdd11c4
Somfic <somfic@gmail.com>
2020-11-23 21:40:39 +0100

    Changed title

---
+++ client/src/components/checkout/CheckoutItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="checkout-item">
+    <p>{{name}}</p>
+    <p>€ {{cost}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckoutItem",
+  props: {
+    name: String,
+    cost: String,
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.checkout-item {
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ client/src/components/checkout/CheckoutTotal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="checkout-total">
+    <p>Total</p>
+    <p>€ {{cost}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckoutTotal",
+  props: {
+    cost: String,
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.checkout-total {
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
