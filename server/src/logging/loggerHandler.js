34f6773a1746ab0752e7bd8685f57561713200e8
Somfic <somfic@gmail.com>
2020-11-26 21:15:04 +0100

    Created logging middleware

---
+++ server/src/logging/loggerHandler.js
@@ -0,0 +1,13 @@
+const logger = require('../logging/logger');
+
+module.exports = (req, res, next) => {
+	let requestInfo = {
+		authorization: req.headers.authorization,
+		query: req.query,
+		body: req.body,
+		params: req.params
+	}
+
+	logger.debug(`Incoming request: ${req.protocol + '://' + req.get('host') + req.originalUrl}`, requestInfo);
+    next();
+}
