64a035ee534e825f840129658c0c11447fde73c6
Somfic <somfic@gmail.com>
2020-11-26 18:36:21 +0100

    Created database service

---
+++ server/src/config/database.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const config = require("./config");
+const logger = require("./logger");
+
+module.exports = {
+    connect() {
+        if (!config.databaseUrl) {
+            logger.warning("Environment variable DB_CONNECTION has not been set");
+        }
+
+        mongoose.connect(
+            config.databaseUrl,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+                useCreateIndex: true,
+            },
+            (err) => {
+                if (err) {
+                    logger.error("Could not connect to database", err);
+                } else {
+                    logger.info("Connected to database");
+                }
+            }
+        );
+    }
+};
