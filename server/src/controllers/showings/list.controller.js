87bfbae5cfc20fc22340361f9eb661966acfd04b
Somfic <somfic@gmail.com>
2020-11-26 21:15:39 +0100

    Update list.controller.js

--- server/src/controllers/showings/list.controller.js
+++ server/src/controllers/showings/list.controller.js
@@ -1,9 +1,17 @@
-const logger = require("../../config/logger");
+const ServerError = require("../../error/ServerError");
+const logger = require("../../logging/logger");
+const Showing = require('../../models/Showing');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     if(req.query.id) { next(); return; }
 
     logger.trace('Controller: showings/list');
 
-    res.json('all showings here');
+    try {
+        let showings = await Showing.find();
+        res.status(200).json(showings);
+        
+    } catch(err) {
+        next(ServerError.badRequest(err));
+    }
 };
