af7545eb65805540dfbad986338cf4bbf67df742
Somfic <somfic@gmail.com>
2020-11-26 21:15:27 +0100

    Update showings.js

--- server/src/routes/showings.js
+++ server/src/routes/showings.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const logger = require('../config/logger');
+const express = require("express");
+const validationHandler = require("../validation/validationHandler");
 
 const router = express.Router();
 
 // List
-router.get('/', require('../controllers/showings/list.controller'));
+router.get("/", require("../controllers/showings/list.controller"));
 
 // Detail
-router.get('/', require('../controllers/showings/detail.controller'));
+router.get(
+	"/detail",
+	validationHandler(require("../controllers/showings/schemas/detail.schema")),
+	require("../controllers/showings/detail.controller")
+);
 
 // Create
-router.post('/', require('../controllers/showings/create.controller'));
+router.post("/", require("../controllers/showings/create.controller"));
 
 module.exports = router;
