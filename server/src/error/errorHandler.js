5ff00bc61c25ce11ff056fff7764cbf079af2667
Somfic <somfic@gmail.com>
2020-11-26 21:14:56 +0100

    Created error middleware

---
+++ server/src/error/errorHandler.js
@@ -0,0 +1,12 @@
+const ServerError = require('./ServerError');
+const logger = require('../logging/logger');
+
+module.exports = (err, req, res, next) => {
+    if(err instanceof ServerError) {
+        logger.error(err.message);
+        return res.status(err.code).json(err.message);
+    }
+
+    logger.error(`Unexpected exception: ${err}`);
+    return res.status(500).json('Something went wrong');
+}
