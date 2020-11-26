4deb0eef62f6ca128080b6581b4d2f719e3108a9
Somfic <somfic@gmail.com>
2020-11-26 21:15:49 +0100

    Update config.js

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,5 +3,6 @@ require('dotenv/config');
 module.exports = {
     mode: process.env.NODE_ENV || 'development',
     databaseUrl: process.env.DB_CONNECTION,
-    logLevel: process.env.LOGLEVEL || 'debug'
+    logLevel: process.env.LOGLEVEL || 'debug',
+    port: process.env.PORT || 3000
 }
