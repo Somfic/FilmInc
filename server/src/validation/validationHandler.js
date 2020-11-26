1bcaf84bbd67566a5096eb573487327a429f6259
Somfic <somfic@gmail.com>
2020-11-26 21:15:22 +0100

    Created validation middleware

---
+++ server/src/validation/validationHandler.js
@@ -0,0 +1,12 @@
+const yup = require('yup');
+const ServerError = require('../error/ServerError');
+
+module.exports = (schema) => async (req, res, next) => {
+    try {
+        let validBody = await schema.validate(req.body);
+        req.body = validBody;
+        next();
+    } catch(err) {
+        next(ServerError.badRequest(err));
+    }
+}
