c1bfd1ce05e9dba3c7d1e1fbd35d9843f473b0eb
Somfic <somfic@gmail.com>
2020-11-28 17:39:48 +0100

    Server and client now both through serverside express

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const config = require("./src/config/config.js");
 const logger = require("./src/logging/logger");
 const database = require("./src/config/database");
+const path = require("path");
 
 // Setup parser
 app.use(require("body-parser").json());
@@ -16,6 +17,14 @@ app.use("/showings", require("./src/routes/showings"));
 
 // Error handling
 app.use(require("./src/error/errorHandler.js"));
+
+// Serve static client
+app.use(express.static(path.resolve(__dirname, "public/")));
+
+// Handle SPA
+app.get("*", (req, res) =>
+	res.sendFile(path.resolve(__dirname, "public/index.html"))
+);
 
 app.listen(config.port, () => {
 	logger.info(`Server initiated on port ${config.port}`);
