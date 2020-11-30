4efba73e4bd7b710b46668850533161eff98c833
Somfic <somfic@gmail.com>
2020-11-30 23:38:18 +0100

    Created components

---
+++ client/src/components/admin/AddWatchable.vue
@@ -0,0 +1,78 @@
+<template>
+	<div class="add-watchable">
+		<div class="preview">
+			<Watchable :title="title"></Watchable>
+			<p>{{ titlemessage }}</p>
+		</div>
+		<form>
+			<div class="row">
+				<div class="form-group col-4">
+					<label for="title">Titel</label>
+					<input
+						v-model="titlemessage"
+						type="text"
+						class="form-control"
+						id="title"
+						placeholder="Titel"
+					/>
+				</div>
+				<div class="form-group col-2">
+					<label for="length">Lengte</label>
+					<div class="input-group">
+						<input
+							v-model="length"
+							type="number"
+							class="form-control"
+							id="length"
+						/>
+						<div class="input-group-append">
+							<div class="input-group-text">minuten</div>
+						</div>
+					</div>
+				</div>
+				<div class="form-group col-6">
+					<label for="poster">Poster afbeelding URL</label>
+					<input
+						v-model="poster"
+						type="text"
+						class="form-control"
+						id="poster"
+					/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="form-group col-8">
+					<label for="description">Beschrijving</label>
+					<textarea
+						v-model="description"
+						type="text"
+						class="form-control"
+						id="description"
+						rows="5"
+					/>
+				</div>
+				<div class="col-4">
+					<div class="form-group">
+						<label for="classification">Klassificatie</label>
+						<input
+							v-model="classification"
+							type="text"
+							class="form-control"
+							id="classification"
+						/>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+</template>
+<script>
+import Watchable from "../watchable/Watchable";
+
+export default {
+	name: "AddWatchable",
+	components: {
+		Watchable,
+	},
+};
+</script>
---
+++ client/src/components/watchable/Watchable.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="card flex-row flex-wrap">
+		<div class="card-header border-0">
+			<img :src="img" alt="" />
+		</div>
+		<div class="card-block px-2">
+			<h4 class="card-title">{{ title }}</h4>
+			<p class="card-text">{{ description }}</p>
+			<a href="#" class="btn btn-primary">BUTTON</a>
+		</div>
+		<div class="w-100"></div>
+		<div class="card-footer w-100 text-muted">FOOTER</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Watchable",
+	props: {
+		title: String,
+		description: String,
+		poster: String,
+		length: Number,
+		classification: String,
+		variations: Array,
+	},
+};
+</script>
