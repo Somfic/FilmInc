7cf367429380a6dfc5f2a6a8b24053ed10ec44d5
Somfic <somfic@gmail.com>
2020-11-30 23:38:47 +0100

    Update TicketSelection.vue

--- client/src/views/TicketSelection.vue
+++ client/src/views/TicketSelection.vue
@@ -1,26 +1,42 @@
 <template>
-	<div class="ticket-selection list-group">
-		<Scheduled
-			img="https://kinepolis.nl/sites/kinepolis.nl/files/styles/kinepolis_filter_movie_poster/public/posters/Tenet_ps_1_jpg_sd-high_Copyright-COPYRIGHT-2020-Warner-Bros-Entertainment-Inc-All-Rights-Reserved-Photo-Credit-Melinda-Sue-Gordon_NL_FO.jpg"
-			title="Tenet"
-			start_time="12:35"
-			end_time="14:21"
-			theatre="5"
-			age="12"
-			tags="2D"
-		>
-		</Scheduled>
+	<div class="ticket-selection row">
+		<div class="schedule list-group col-auto">
+			<Scheduled
+				v-for="item in watchables"
+				:key="item"
+				:img="item.poster"
+				:title="item.title"
+			>
+			</Scheduled>
+		</div>
+		<div class="checkout col-auto">
+			<Checkout></Checkout>
+		</div>
 	</div>
 </template>
 
 
 <script>
 import Scheduled from "../components/Scheduled";
+import Checkout from "../components/checkout/CheckoutItem";
+import watchableService from "../services/watchable";
 
 export default {
 	name: "TicketSelection",
 	components: {
 		Scheduled,
+		Checkout,
+	},
+	data() {
+		return { watchables: [] };
+	},
+	async created() {
+		try {
+			let request = await watchableService.get();
+			this.watchables = request.data;
+		} catch (err) {
+			console.log(err);
+		}
 	},
 };
 </script>
