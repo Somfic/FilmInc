0b16dc1118f06284056134079774bbb814dc7b2c
Somfic <Somfic@users.noreply.github.com>
2020-12-07 02:59:20 +0000

    deploy: 09f58d65ddda6663211422fdd5ff5387198432f6

--- src/controllers/scheduled/updateMany.controller.js
+++ src/controllers/scheduled/updateMany.controller.js
@@ -6,8 +6,17 @@ module.exports = async(req, res, next) => {
     logger.trace("Controller: scheduled.updateMany");
 
     try {
-        let result = await Scheduled.updateMany(req.body);
-        res.status(200).json(result);
+        let results = [];
+
+        req.body.forEach(async(item) => {
+            results.push(
+                await Scheduled.findByIdAndUpdate(item._id, item, {
+                    upsert: true,
+                })
+            );
+        });
+
+        res.status(200).json(results);
     } catch (err) {
         next(ServerError.badRequest(err));
     }
