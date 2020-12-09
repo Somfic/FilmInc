a36354edd9fee7267cd7f0b159fb5ce661c036e8
Somfic <Somfic@users.noreply.github.com>
2020-12-09 18:04:10 +0000

    deploy: 0b4c1da34358056c802d96056381c1793985754d

---
+++ public/js/chunk-2d2308ce.a9209c18.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2308ce"],{ed81:function(n,c,e){"use strict";e.r(c);const o={};c["default"]=o}}]);
+//# sourceMappingURL=chunk-2d2308ce.a9209c18.js.map
