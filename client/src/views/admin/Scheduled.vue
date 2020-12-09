38f66442c430e7f5a8372673f942a771a095c188
Somfic <somfic@gmail.com>
2020-12-09 13:33:29 +0100

    Update Scheduled.vue

--- client/src/views/admin/Scheduled.vue
+++ client/src/views/admin/Scheduled.vue
@@ -1,21 +1,17 @@
 <template>
-	<div class="scheduled">
-		<Navbar></Navbar>
-		<Timeline
-			:items="items"
-			@selected="selected"
-			@new="newItem"
-			@updated="update"
-			class="mb-3"
-		></Timeline>
-		<EditScheduled
-			v-if="scheduled.location != 0"
-			:populateWith="scheduled"
-			@updated="update"
-		></EditScheduled>
-		<Action :actions="actions"></Action>
-		<Footer></Footer>
-	</div>
+  <div class="scheduled">
+    <Navbar></Navbar>
+    <Timeline
+      :items="items"
+      @selected="selected"
+      @new="newItem"
+      @updated="update"
+      class="mb-3"
+    ></Timeline>
+    <EditScheduled :populateWith="scheduled" @updated="update"></EditScheduled>
+    <Action :actions="actions"></Action>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
@@ -26,43 +22,43 @@ import Navbar from "../../components/navbar/Navbar";
 import scheduledService from "../../services/scheduled";
 
 export default {
-	name: "Scheduled",
-	components: {
-		Timeline,
-		EditScheduled,
-		Navbar,
-		Footer,
-	},
-	data() {
-		return {
-			items: [],
-			scheduled: {},
-			actions: [
-				{
-					message: "Sla wijzigingen op",
-					class: "btn-dark",
-					action: () => scheduledService.editMany(this.items),
-				},
-			],
-		};
-	},
-	async created() {
-		this.update();
-	},
-	methods: {
-		selected(item) {
-			console.log("selected", item);
-			this.scheduled = item;
-		},
-		newItem(item) {
-			item.isUpdated = true;
-			this.items.push(item);
-		},
-		update() {
-			scheduledService.get().then((res) => {
-				this.items = res.data;
-			});
-		},
-	},
+  name: "Scheduled",
+  components: {
+    Timeline,
+    EditScheduled,
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      items: [],
+      scheduled: {},
+      actions: [
+        {
+          message: "Sla wijzigingen op",
+          class: "btn-dark",
+          action: () => scheduledService.editMany(this.items),
+        },
+      ],
+    };
+  },
+  async created() {
+    this.update();
+  },
+  methods: {
+    selected(item) {
+      console.log("selected", item);
+      this.scheduled = item;
+    },
+    newItem(item) {
+      item.isUpdated = true;
+      this.items.push(item);
+    },
+    update() {
+      scheduledService.get().then((res) => {
+        this.items = res.data;
+      });
+    },
+  },
 };
 </script>
