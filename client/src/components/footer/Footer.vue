b6b90aa58cb94fd5690ae09bbd77bba8dc40aeb5
Somfic <somfic@gmail.com>
2020-12-09 13:33:26 +0100

    Update Footer.vue

--- client/src/components/footer/Footer.vue
+++ client/src/components/footer/Footer.vue
@@ -1,32 +1,35 @@
 <template>
-	<div class="ftr rounded border my-3 bg-light p-3">
-		<span class="text-muted">FilmInc 2020</span>
-		<span class="text-muted">{{ name() }}</span>
-	</div>
+  <div class="w-100 rounded border my-3 bg-light p-3 text-center">
+    <div class="row">
+      <div class="col">
+        <span class="text-muted">FilmInc</span>
+      </div>
+
+      <div class="col">
+        <span class="text-muted">{{ name() }}</span>
+      </div>
+
+      <div class="col">
+        <span class="text-muted">Lucas de Jong - 2158725</span>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import cacheService from "../../services/cache";
 import jwt from "jsonwebtoken";
 
 export default {
-	name: "Footer",
-	data() {
-		return {
-			name: () => {
-				let token = cacheService.read("token");
-				let payload = jwt.decode(token);
+  name: "Footer",
+  data() {
+    return {
+      name: () => {
+        let token = cacheService.read("token");
+        let payload = jwt.decode(token);
 
-				return payload.name.toString();
-			},
-		};
-	},
+        return payload.name.toString();
+      },
+    };
+  },
 };
 </script>
-<style scoped>
-.ftr {
-	display: flex;
-	align-items: center;
-	justify-content: space-around;
-	width: 100%;
-}
-</style>
